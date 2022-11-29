cf8fa14ef60360f0ffb458c8ed98830faa53f15a
JinGyuGwak <wlsrb2695@naver.com>
2022-11-29 17:45:51 +0900

    회원도메인개발

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,25 +2,51 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
-    @Autowired
-    private MemberRepository memberRepository;
+
+    private final MemberRepository memberRepository;
+
+//    @RequiredArgsConstructor 를 사용하면 아래 생성자 주입 안 써도 자동으로 해줌
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     //회원 가입
+    @Transactional
     public Long join(Member member){
-        validateDuplicateMember(member);
+        validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
-    private void validateDuplicateMember(Member member){
-        //EXCEPTION 예외
 
+    //EXCEPTION 예외
+    private void validateDuplicateMember(Member member){
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 
     //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    //회원 1개 조회
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
 
 }
