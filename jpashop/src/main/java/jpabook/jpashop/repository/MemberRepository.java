7d735cd9a93f4e8093b83d3a22a56a1d7dc7c4b8
JinGyuGwak <wlsrb2695@naver.com>
2023-02-06 20:45:12 +0900

    스프링데이터JPA

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -10,24 +11,7 @@ import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    List<Member> findByName(String name);
 
-    private final EntityManager em;
-
-    public void save(Member member){
-        em.persist(member);
-    }
-    public Member findOne(Long id){
-        return em.find(Member.class, id);
-    }
-    public List<Member> findAll(){
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-    public List<Member> findByName(String name){
-        return em.createQuery("select  m from Member m where m.name = :name", Member.class)
-                .setParameter("name",name)
-                .getResultList();
-    }
 }
