ac2b8cc72881a1938602c789acb87ac78c66e70f
JinGyuGwak <104514223+JinGyuGwak@users.noreply.github.com>
2023-04-11 22:36:08 +0900

    0411 JPA 섹션3, 네트워크

--- jpaOneCycle/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpaOneCycle/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,14 +2,35 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
-        em.close();
+
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            //비영속 상태
+            Member member1 = new Member(4L,"Hello");
+            Member member2 = new Member(5L, "B");
+
+            //영속 상태
+            em.persist(member1);
+            em.persist(member2);
+
+
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
 
         System.out.println("실행이 되었어요");
