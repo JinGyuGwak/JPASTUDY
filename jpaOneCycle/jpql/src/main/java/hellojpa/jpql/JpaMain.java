9c9ec505610aba351bc0e4da3efd62f20de2524e
JinGyuGwak <104514223+JinGyuGwak@users.noreply.github.com>
2023-04-22 01:39:10 +0900

    0422 스프링데이터 jpa 시작

--- jpaOneCycle/jpql/src/main/java/hellojpa/jpql/JpaMain.java
+++ jpaOneCycle/jpql/src/main/java/hellojpa/jpql/JpaMain.java
@@ -29,33 +29,28 @@ public class JpaMain {
             Member member1 = new Member();
             member1.setUsername("member1");
             member1.setTeam(team1);
+            member1.setAge(20);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("member2");
             member2.setTeam(team1);
+            member2.setAge(20);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("member3");
             member3.setTeam(team2);
+            member3.setAge(20);
             em.persist(member3);
 
 
             em.flush();
             em.clear();
 
-            String jpql = "select distinct t from Team t join fetch t.members";
+            int resultCount = em.createQuery("update Member m set m.age=30")
+                    .executeUpdate();
 
-            List<Team> result = em.createQuery(jpql, Team.class)
-                    .getResultList();
-            for (Team team : result) {
-                System.out.println("team = " + team.getName() +
-                        " | memberSize = " + team.getMembers().size());
-                for(Member member : team.getMembers()){
-                    System.out.println("-> members = " + member);
-                }
-            }
 
             tx.commit();
         } catch (Exception e){
